205ca6c9f31423cf51604f37b4f1018875db3c4e
Moni <Monika.Deinzer1@gmail.com>
2022-08-17 12:30:25 +0200

    Added a score keeping system

--- script.js
+++ script.js
@@ -5,24 +5,39 @@ const plays=["Rock", "Paper", "Scissors"];
 function computerPlay() {
      return plays[Math.floor(Math.random() * plays.length)];
  }
-
+ 
 // Prompt is displayed to ask the player for their input and is then displayed
 function playerPlay() {
     let playerInput = window.prompt("What's your play?");
     return playerInput[0].toUpperCase() + playerInput.toLowerCase().substring(1);
 }
 
+// Variables storing the scores
+var playerScore = 0;
+var computerScore = 0;
+var ties = 0;
+
 // Function to play one round
 function playRound(playerSelection, computerSelection) {
+    let computerWinsRound = "You lost... "+playerSelection+" beats "+computerSelection.toLowerCase();
+    let playerWinsRound = "You won! "+computerSelection+" beats "+playerSelection.toLowerCase();
+    let tie = "It's a tie!";
     if (playerSelection === computerSelection) {
-        return greeting = ("It's a tie!");
+        ties++;
+        return tie;
     } else {
         if (playerSelection === "Scissors" && computerSelection === "Rock" || playerSelection === "Paper" && computerSelection === "Scissors" || playerSelection === "Rock" && computerSelection === "Paper") {
-            return "You lost... " + computerSelection + " beats " + playerSelection;
+            playerScore++;
+            return playerWinsRound;
         } else {
-                return "You won! " + playerSelection + " beats " + computerSelection;
-                }
+            if (computerSelection === "Scissors" && playerSelection === "Rock" || computerSelection === "Paper" && playerSelection === "Scissors" || computerSelection === "Rock" && playerSelection === "Paper") {
+                computerScore++;
+                return computerWinsRound;
+             } else {
+                return "Hmm, something went wrong here";
             }
+        }
+    }
 }
 
 // Function to play five rounds
@@ -31,11 +46,28 @@ function game() {
       let computerSelection = computerPlay();
       let playerSelection = playerPlay();
       let result = playRound(computerSelection, playerSelection);
-      console.log("Computer chose " + computerSelection + " in round " + i);
-      console.log("Player chose " + playerSelection + " in round " + i);
+      console.log("Computer chose " + computerSelection.toLowerCase() + " in round " + i);
+      console.log("Player chose " + playerSelection.toLowerCase() + " in round " + i);
       console.log("Round " + i + ": " + result);
+      console.log("Player score: "+playerScore);
+      console.log("Computer score: "+computerScore);
+      console.log("Ties: "+ties);
     }
  }
 
 game() 
 
+// Announces the final winner
+function determineWinner() {
+    if (computerScore > playerScore){
+        return "Computer wins this one. Better luck next time!";
+    } else {
+        if (playerScore > computerScore) {
+            return "Congrats! You beat the machine."
+        } else {
+            return "Game's over. It's a tie!"
+        }
+    }
+}
+
+console.log(determineWinner())
